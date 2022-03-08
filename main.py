77e7d72ccaf1cbd87ad8110b6661417cdf47d84e
sexozavr <daniildiveev@mail.ru>
2022-03-09 00:02:52 +0300

    bot can now encode images, nevertheless the decoding process has some stupid ass bug

--- main.py
+++ main.py
@@ -16,27 +16,6 @@ def send_hello_message(message):
                     f'Hello, {user.username}! This bot can extend png files with the prefered text without changing the look of the original image.')
 
 
-@bot.message_handler(content_types='photo')
-def get_image(message):
-    fileID = message.photo[-1].file_id
-    file_info = bot.get_file(fileID)
-    downloaded_file = bot.download_file(file_info.file_path)
-
-    image_path = f"images/{fileID}.png"
-
-    with open(image_path, 'wb') as new_file:
-        new_file.write(downloaded_file)
-        new_file.close()
-
-    image_ok = True
-
-
-@bot.message_handler(content_types='text')
-def get_text(message):
-    message_to_encode = message.json.text
-    text_ok = True
-
-
 @bot.message_handler(commands=['encode'])
 def hide_message_in_png(message):
     global image_ok, text_ok
@@ -53,6 +32,57 @@ def hide_message_in_png(message):
                 f.close()
 
         image_ok, text_ok = False, False
+
+    elif not image_ok and not text_ok:
+        bot.send_message(message.chat.id, "Both of your text and your image are missing")
+    elif not image_ok:
+        bot.send_message(message.chat.id, "You haven't uploaded any images yet")
+    elif not text_ok:
+        bot.send_message(message.chat.id, "You haven't sent me any text messages yet")
+
+@bot.message_handler(commands=['decode'])
+def decode_image(message):
+    global image_ok, image_path
+
+    if image_ok:
+        result = retrieve_message_from_png(image_path)
+
+        if isinstance(result, int):
+            bot.send_message(message.chat.id, "There are no secret messages in this image")
+        else:
+            bot.send_message(message.chat.id, "Found secret message!! Here it is:")
+            bot.send_message(message.chat.id, result)
+
+        image_ok = False
+
+    else:
+        bot.send_message(message.chat.id, "Seems like you haven't uploaded any images yet")
+
+
+@bot.message_handler(content_types='photo')
+def get_image(message):
+    global image_ok, image_path
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_path = f"images/{fileID}.png"
+
+    with open(image_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
+        new_file.close()
+
+    bot.send_message(message.chat.id, "Got your image!")
+
+    image_ok = True
+
+
+@bot.message_handler(content_types='text')
+def get_text(message):
+    global text_ok, message_to_encode
+    message_to_encode = message.json['text']
+    bot.send_message(message.chat.id, f"Your secret message is: {message_to_encode}")
+    text_ok = True
 
 if __name__ == '__main__':
     if not os.path.exists('images'):
